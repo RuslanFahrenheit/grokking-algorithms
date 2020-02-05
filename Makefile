fd128411d9d3fe0841a27e1e2fc1572c1618bbaa
Ruslan Kim <rkim@justcoded.co>
2020-02-05 14:53:14 +0200

    add documentation package
    configure the documentation package

--- Makefile
+++ Makefile
@@ -6,8 +6,13 @@ build:
 	mkdir dist
 	npm run build
 
+docs:
+	npm run documentation -- build src/index.js -f md > docs/README.md
+
 test:
 	npm run test
 
 lint:
 	npx eslint .
+
+.PHONY: test docs
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/**
+ * Sum two numbers.
+ * @param {number} a any number
+ * @param {number} b any number
+ * @returns {number} sum of that numbers.
+ */
 const sum = (a, b) => a + b;
 
 export default sum;
